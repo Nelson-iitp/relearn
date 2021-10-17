03cefb83a35bbe3791f6bcf73a2bc7163909118f
Nelson-iitp <nelson_2121cs07@iitp.ac.in>
2021-10-17 07:11:09 +0530

    a1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name =          'relearn',
-    version =       '0.0.3',  # 0.0.x is for unstable versions
+    version =       '0.0.4',  # 0.0.x is for unstable versions
     url =           "https://github.com/Nelson-iitp/relearn",
     author =        "Nelson.S",
     author_email =  "nelson_2121cs07@iitp.ac.in",
--- src/relearn/__init__.py
+++ src/relearn/__init__.py
@@ -1,4 +1,5 @@
-
+import relearn.pies
+import explore
 print(
 """ 
     ~ R E L E A R N ~
